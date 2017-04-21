287e8cad5d450f79eadb7e2475c1aa9b64c7a36a
Lennie <lennie@lempa.se>
2017-04-21 11:48:31 +0200

    test

---
+++ zsbsCode/TestConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("test");
+            Console.ReadLine();
+        }
+    }
+}
